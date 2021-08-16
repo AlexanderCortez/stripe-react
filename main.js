b302560c1bf6d033663b8d3c8e9dd46a7cbd1cf9
AlexanderCortez <joelalexander10@outlook.com>
2021-08-16 16:51:30 -0600

    ✨ Stripe setup

---
+++ main.js
@@ -0,0 +1,61 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const stripeInit = require("stripe");
+
+const { PORT, STRIPE_KEY } = process.env;
+
+const stripe = stripeInit(STRIPE_KEY);
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/api/payment/create-payment-intent", async (req, res) => {
+  const { amount, currency, paymentMethod } = req.body || {};
+
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+      payment_method_types: [paymentMethod],
+    });
+    res.send({ data: { clientSecret: paymentIntent.client_secret } });
+  } catch (err) {
+    res.status(err?.statusCode || 400).send({
+      error: {
+        statusCode: err?.statusCode || 400,
+        message: err?.raw?.message || "Random error",
+      },
+    });
+  }
+});
+
+app.post("/api/payment/create-payment-method", async (req, res) => {
+  const { type, cardNumber, expMonth, expYear, cvc } = req.body || {};
+
+  try {
+    const paymentMethod = await stripe.paymentMethods.create({
+      type,
+      card: {
+        number: cardNumber,
+        exp_month: expMonth,
+        exp_year: expYear,
+        cvc,
+      },
+    });
+    res.send({ data: paymentMethod });
+  } catch (err) {
+    res.status(err?.statusCode || 400).send({
+      error: {
+        statusCode: err?.statusCode || 400,
+        message: err?.raw?.message || "Random error",
+      },
+    });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
